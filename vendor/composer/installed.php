f73c991c91e9d6c4b4939f83afb424b6362608ac
aryayudh06 <aryayudhakp@gmail.com>
2024-06-21 18:02:18 +0700

    Changes to be committed: 	new file:   .github/workflows/deploy.yml 	modified:   vendor/composer/installed.php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '327aafcba0c2ac39e1407b25b99672b8bfb3ffad',
+        'reference' => '74fb2aa5ffa7fa3cb3ebf979f120dbdd720729d8',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -391,7 +391,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '327aafcba0c2ac39e1407b25b99672b8bfb3ffad',
+            'reference' => '74fb2aa5ffa7fa3cb3ebf979f120dbdd720729d8',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
